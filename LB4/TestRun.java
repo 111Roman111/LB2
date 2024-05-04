54cd4e40b450a95a941a5f1de4a07429af6345a4
111Roman111 <163007917+111Roman111@users.noreply.github.com>
2024-05-04 16:08:32 +0300

    Add files via upload

---
+++ LB4/TestRun.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import edu.ntudp.lab4.JsonManager;
+import edu.ntudp.lab3.Run;
+import edu.ntudp.lab3.model.University;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class TestRun {
+
+    @Test
+    void createTypicalUniversity() {
+        University university = new Run().createTypicalUniversity();
+        JsonManager jsonManager = new JsonManager();
+        try {
+            jsonManager.writeToFileJson(university, "university.json");
+        } catch (IOException e) {
+            fail(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        University university2;
+        try {
+            university2 = jsonManager.readFromFileJson("university.json", University.class);
+        } catch (FileNotFoundException e) {
+            fail(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(university, university2);
+
+    }
+}
